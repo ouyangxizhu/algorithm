1c685c205b7c2bad8908485bfd0820e1b2bcc13c
zhangchangzhen <zhangchangzhen@meituan.com>
2021-03-02 22:46:33 +0800

    feat:finish

--- src/bytedance/NC34.java
+++ src/bytedance/NC34.java
@@ -1,5 +1,8 @@
 package bytedance;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class NC34 {
     /**
      *
@@ -9,14 +12,16 @@ public class NC34 {
      */
     public int uniquePaths (int m, int n) {
         int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
+                if (j == 0) {
                     dp[i][j] = 1;
                 } else {
-
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
         }
+        return dp[m - 1][n - 1];
     }
 }
